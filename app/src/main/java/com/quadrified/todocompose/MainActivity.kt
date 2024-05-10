68e86a4912e1330964f0a31561c1c0940c030509
Omer Quadri <omerquadri01@gmail.com>
2024-05-10 16:18:25 -0400

    Add Project Dependencies - May/2024

--- app/src/main/java/com/quadrified/todocompose/MainActivity.kt
+++ app/src/main/java/com/quadrified/todocompose/MainActivity.kt
@@ -16,32 +16,10 @@ import com.quadrified.todocompose.ui.theme.TodoComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TodoComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodoComposeTheme {
-        Greeting("Android")
     }
 }
