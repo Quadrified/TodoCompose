8c7dad7c719105d9af23c494e3b2af10cb484b87
Omer Quadri <omerquadri01@gmail.com>
2024-05-10 18:41:23 -0400

    Set up Room Database and Queries

---
+++ app/src/main/java/com/quadrified/todocompose/data/ToDoDatabase.kt
@@ -0,0 +1,10 @@
+package com.quadrified.todocompose.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.quadrified.todocompose.data.models.ToDoTask
+
+@Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase : RoomDatabase() {
+    abstract fun toDoDao(): ToDoDao
+}
--- app/src/main/java/com/quadrified/todocompose/data/models/ToDoTask.kt
+++ app/src/main/java/com/quadrified/todocompose/data/models/ToDoTask.kt
@@ -4,10 +4,10 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.quadrified.todocompose.utils.Constants
 
-@Entity(tableName = Constants.DATABASE_TABLE)
+
+@Entity(tableName = Constants.DATABASE_TABLE) // Entity => Table
 data class ToDoTask(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val description: String,
     val priority: Priority
